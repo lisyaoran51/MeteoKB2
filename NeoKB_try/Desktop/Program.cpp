9e7980bf589faca221de1647278ff01541857b5f
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2019-10-31 14:56:04 +0800

    no message

--- NeoKB_try/Desktop/Program.cpp
+++ NeoKB_try/Desktop/Program.cpp
@@ -3,12 +3,14 @@
 #include "../Framework/Host/MeteoGameHost.h"
 #include "MeteoGameDesktop.h"
 #include "../Instruments/MeteoPiano.h"
+#include "../Util/Log.h"
 
 
 
 using namespace Desktop;
 using namespace Framework::Host;
 using namespace Instruments;
+using namespace Util;
 
 
 Program::Program(){}
@@ -16,6 +18,8 @@ Program::Program(){}
 
 int Program::Main(vector<string>& args)
 {
+	Util::logLevel = LogLevel::Info;
+
 	GameHost* host = new MeteoGameHost();
 
 	host->Run(new MeteoGameDesktop(args), new MeteoPiano(args));
