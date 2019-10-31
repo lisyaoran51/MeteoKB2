76f531c6795bd969b079ebdfdf0480a40c26f134
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2019-10-31 15:23:24 +0800

    no message

--- NeoKB_try/Framework/Audio/AudioCollectionManager.h
+++ NeoKB_try/Framework/Audio/AudioCollectionManager.h
@@ -25,16 +25,29 @@ namespace Audio {
 
 	public:
 
-		int AddItem(T* item);
+		int AddItem(T* item) {
+			RegisterItem(item);
+			AddItemToList(item);
+			return 0;
+		}
 
-		int AddItemToList(T* item);
+		int AddItemToList(T* item) {
+			items.push_back(item);
+			return 0;
+		}
 
 		/// <summary>
 		/// 懶得寫action，所以還沒有用
 		/// </summary>
-		int RegisterItem(T* item);
+		int RegisterItem(T* item) {
+			// 懶得寫
+			return 0;
+		}
 
-		int UnregisterItem(T* item);
+		int UnregisterItem(T* item) {
+			// 懶得寫
+			return 0;
+		}
 
 		
 
@@ -42,12 +55,32 @@ namespace Audio {
 
 		vector<T*> items;
 
-		virtual int deleteItem(T* item);
+		virtual int deleteItem(T* item) {
+			return 0;
+		}
 
 		/// <summary>
 		/// 這個會把他下面的其他manager更新，然後audio manager會把這個update放到thread裡面跑
 		/// </summary>
-		virtual int update();
+		virtual int update() {
+			AdjustableAudioComponent::update();
+
+			for (int i = 0; i < items.size(); i++) {
+				AdjustableAudioComponent* item = dynamic_cast<AdjustableAudioComponent*>(items[i]);
+
+				if (item == nullptr)
+					throw runtime_error("AudioCollectionManager<T>::update: this component's type is wrong.");
+
+				if (item->GetIsCompleted()) {
+					items.erase(items.begin() + i);
+					i--;
+					deleteItem(item);
+					// smaple不用delete，但是track要delete
+				}
+			}
+
+			return 0;
+		}
 
 	private:
 
