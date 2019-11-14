e422f893225969d22be3077eb015449108acff1a
a0939946923 <a0939946923@gmail.com>
2019-11-14 14:42:12 +0800

    no message

--- NeoKB_try/Desktop/Devices/bcm2835.c
+++ NeoKB_try/Desktop/Devices/bcm2835.c
@@ -1331,7 +1331,7 @@ int bcm2835_init(void)
     }
 	
     /* Base of the peripherals block is mapped to VM */
-    bcm2835_peripherals = mapmem("gpio", bcm2835_peripherals_size, memfd, (uint32_t)bcm2835_peripherals_base);
+    bcm2835_peripherals = (uint32_t*)mapmem("gpio", bcm2835_peripherals_size, memfd, (uint32_t)bcm2835_peripherals_base);
     if (bcm2835_peripherals == MAP_FAILED) goto exit;
 
     /* Now compute the base addresses of various peripherals, 
@@ -1365,15 +1365,15 @@ int bcm2835_close(void)
     if (debug) return 1; /* Success */
 
     unmapmem((void**) &bcm2835_peripherals, bcm2835_peripherals_size);
-    bcm2835_peripherals = MAP_FAILED;
-    bcm2835_gpio = MAP_FAILED;
-    bcm2835_pwm  = MAP_FAILED;
-    bcm2835_clk  = MAP_FAILED;
-    bcm2835_pads = MAP_FAILED;
-    bcm2835_spi0 = MAP_FAILED;
-    bcm2835_bsc0 = MAP_FAILED;
-    bcm2835_bsc1 = MAP_FAILED;
-    bcm2835_st   = MAP_FAILED;
+    bcm2835_peripherals = (uint32_t*)MAP_FAILED;
+    bcm2835_gpio = (uint32_t*)MAP_FAILED;
+    bcm2835_pwm  = (uint32_t*)MAP_FAILED;
+    bcm2835_clk  = (uint32_t*)MAP_FAILED;
+    bcm2835_pads = (uint32_t*)MAP_FAILED;
+    bcm2835_spi0 = (uint32_t*)MAP_FAILED;
+    bcm2835_bsc0 = (uint32_t*)MAP_FAILED;
+    bcm2835_bsc1 = (uint32_t*)MAP_FAILED;
+    bcm2835_st   = (uint32_t*)MAP_FAILED;
     return 1; /* Success */
 }    
 
