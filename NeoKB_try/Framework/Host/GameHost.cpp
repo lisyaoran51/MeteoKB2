54316e79667efaa51c9c8dad54c8485e20905ea5
a0939946923 <a0939946923@gmail.com>
2019-11-18 02:25:00 +0800

    no message

--- NeoKB_try/Framework/Host/GameHost.cpp
+++ NeoKB_try/Framework/Host/GameHost.cpp
@@ -68,13 +68,13 @@ int GameHost::Run(Game* game, Instrument* instrument)
 
 	LOG(LogLevel::Finest) << "Initialize Threads.";
 	inputInitialize();
-	inputThread->Start();
+	//inputThread->Start();
 
 	drawInitialize();
-	drawThread->Start();
+	//drawThread->Start();
 
 	updateInitialize();
-	updateThread->Start();
+	//updateThread->Start();
 
 	bootstrapSceneGraph(game, instrument);
 
