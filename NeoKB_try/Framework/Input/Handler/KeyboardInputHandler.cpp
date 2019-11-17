08426257e9aa16d6ef359d04ef15d29c2c264e2b
a0939946923 <a0939946923@gmail.com>
2019-11-18 00:48:38 +0800

    no message

--- NeoKB_try/Framework/Input/Handler/KeyboardInputHandler.cpp
+++ NeoKB_try/Framework/Input/Handler/KeyboardInputHandler.cpp
@@ -15,6 +15,10 @@ int KeyboardInputHandler::Initialize(GameHost * host)
 {
 	LOG(LogLevel::Info) << "KeyboardInputHandler::Initialize() : adding handler.";
 
+	vector<pair<uintptr_t, string>>* callbackKeys = host->GetMainInterface()->GetKeyboard()->_DebugGetActionList()->_DebugGetCallbackKeys();
+
+	LOG(LogLevel::Finest) << "TestCaseKeyboard : callback keys size = " << callbackKeys->size();
+
 	// 當mian interface讀到輸入時，就會call handle key down來處理
 	host->GetMainInterface()->GetKeyboard()->
 		AddOnKeyEvent<KeyboardInputHandler>(this, bind((int(KeyboardInputHandler::*)(InputState*))&KeyboardInputHandler::HandleState, this, placeholders::_1), "KeyboardInputHandler::HandleState");
