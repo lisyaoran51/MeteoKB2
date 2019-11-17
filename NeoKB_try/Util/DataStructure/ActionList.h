306db406a617110b2510ace185f1af540dacb4b1
a0939946923 <a0939946923@gmail.com>
2019-11-17 22:03:41 +0800

    no message

--- NeoKB_try/Util/DataStructure/ActionList.h
+++ NeoKB_try/Util/DataStructure/ActionList.h
@@ -42,9 +42,9 @@ namespace DataStructure {
 
 			LOG(LogLevel::Finest) << "ActionList::Add() : key = " << key.first << ", name = " << key.second;
 
-			callbackKeys.push_back(key);
+			LOG(LogLevel::Finest) << "ActionList::Add() : key list size:" << callbackKeys.size();
 
-			LOG(LogLevel::Finest) << "ActionList::Add() : push callback";
+			callbackKeys.push_back(key);
 
 			callbacks.push_back(callback);
 
