be4c31ee7db3fe6b1a40e34a41b215459f057fe9
a0939946923 <a0939946923@gmail.com>
2019-11-27 03:00:47 +0800

    no message

--- NeoKB_try/Framework/Timing/DecoupledInterpolatingFramedClock.cpp
+++ NeoKB_try/Framework/Timing/DecoupledInterpolatingFramedClock.cpp
@@ -10,7 +10,7 @@ using namespace Util;
 
 DecoupledInterpolatingFramedClock::DecoupledInterpolatingFramedClock(): InterpolatingFramedClock()
 {
-	LOG(LogLevel::Debug) << "DecoupledInterpolatingFramedClock::DecoupledInterpolatingFramedClock() : address = [" << this << "].";
+	LOG(LogLevel::Debug) << "DecoupledInterpolatingFramedClock::DecoupledInterpolatingFramedClock() : rate = [" << GetRate() << "].";
 	decoupledClock = new FramedClock(decoupledStopwatchClock = new StopwatchClock());
 }
 
