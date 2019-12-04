0410a0177308f949e6ac7250b18375af82586240
a0939946923 <a0939946923@gmail.com>
2019-12-04 11:54:06 +0800

    no message

--- NeoKB_try/Framework/Timing/DecoupledInterpolatingFramedClock.cpp
+++ NeoKB_try/Framework/Timing/DecoupledInterpolatingFramedClock.cpp
@@ -88,9 +88,10 @@ int DecoupledInterpolatingFramedClock::ProcessFrame()
 		else
 			decoupledStopwatchClock->Stop();
 		decoupledStopwatchClock->Seek(GetCurrentTime());
-		LOG(LogLevel::Debug) << "DecoupledInterpolatingFramedClock::ProcessFrame() : current time is [" << fixed << setprecision(5) << GetCurrentTime() << "].";
+		
 	}
 	else {
+		LOG(LogLevel::Debug) << "DecoupledInterpolatingFramedClock::ProcessFrame() : current time is [" << fixed << setprecision(5) << GetCurrentTime() << "].";
 		if (decoupledClock->GetIsRunning()) {
 			//if we're running but our source isn't, we should try a seek to see if it's capable to switch to it for the current value.
 			// 這邊是打開遊戲音樂的地方
