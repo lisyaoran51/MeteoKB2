c22334075dbd6446d8bf1ba2a4e0585f8c3cb91f
a0939946923 <a0939946923@gmail.com>
2019-12-16 15:04:15 +0800

    no message

--- NeoKB_try/Games/Scheduler/Event/EventProcessorMaster.cpp
+++ NeoKB_try/Games/Scheduler/Event/EventProcessorMaster.cpp
@@ -241,7 +241,7 @@ int EventProcessorMaster::update()
 			dynamicEventProcessors.erase(iter);
 			iter--;
 
-			LOG(LogLevel::Depricated) << "EventProcessorMaster::update : step 3 delete.";
+			LOG(LogLevel::Debug) << "EventProcessorMaster::update : step 3 delete.";
 			// TODO: 這邊會有thread safe的問題，要lock
 			Event* e = ep->GetEvent();
 			delete ep;
