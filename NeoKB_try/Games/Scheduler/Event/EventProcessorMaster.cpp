26bdb5182446e69bcbef18f4981c0f9f5751d408
a0939946923 <a0939946923@gmail.com>
2019-12-17 02:59:09 +0800

    no message

--- NeoKB_try/Games/Scheduler/Event/EventProcessorMaster.cpp
+++ NeoKB_try/Games/Scheduler/Event/EventProcessorMaster.cpp
@@ -206,7 +206,7 @@ int EventProcessorMaster::update()
 
 	processEvent(GetClock()->GetElapsedFrameTime()); // 這個是舊的程式，以後可能用不到了
 
-	return 0;
+	//return 0;
 
 	bool isDeleting = false;
 	if(dynamicEventProcessors.size() > 0)
