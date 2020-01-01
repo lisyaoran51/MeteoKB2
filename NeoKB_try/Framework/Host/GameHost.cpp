503ae13164eaa005d2f84ab420fe23ec2f99466a
a0939946923 <a0939946923@gmail.com>
2020-01-01 22:54:11 +0800

    no message

--- NeoKB_try/Framework/Host/GameHost.cpp
+++ NeoKB_try/Framework/Host/GameHost.cpp
@@ -276,7 +276,7 @@ int GameHost::setupConfig()
 	frameworkConfigManager->Set(FrameworkSetting::BlackKeyTargetHeight, 10);	// 14
 	frameworkConfigManager->Set(FrameworkSetting::StartPitch, 24);
 	frameworkConfigManager->Set(FrameworkSetting::FrameRate, 30);
-	frameworkConfigManager->Set(FrameworkSetting::SongTitle, string("GirlTellMe_Simple"));
+	frameworkConfigManager->Set(FrameworkSetting::SongTitle, string("Period_Simple"));
 
 	dependencies->Cache<FrameworkConfigManager>(frameworkConfigManager);
 	
