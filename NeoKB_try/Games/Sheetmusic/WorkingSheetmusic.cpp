ca943bc82581c7253ed28a9d88a797497fc1b054
a0939946923 <a0939946923@gmail.com>
2020-02-02 23:29:39 +0800

    no message

--- NeoKB_try/Games/Sheetmusic/WorkingSheetmusic.cpp
+++ NeoKB_try/Games/Sheetmusic/WorkingSheetmusic.cpp
@@ -69,7 +69,7 @@ Track * WorkingSm::GetTrack()
 	//ifstream* stream = new ifstream(getPathForFile(smInfo->metadata->AudioFile));
 	string path = getPathForFile(smInfo->metadata->AudioFile);
 	char * temp = new char[path.length()];
-	LOG(LogLevel::Depricated) << "WorkingSm::GetTrack() : new a char array.";
+	LOG(LogLevel::Fine) << "WorkingSm::GetTrack() : new a char array.";
 	strcpy(temp, path.c_str());
 	LOG(LogLevel::Fine) << "WorkingSm::GetTrack() : get file path = [" << temp << "].";
 	track = new RateSettableBassTrack(temp);
