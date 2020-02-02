8cfedc507b73f0154e3ed8463ca6b7b84d79d1cc
a0939946923 <a0939946923@gmail.com>
2020-02-02 21:00:27 +0800

    no message

--- NeoKB_try/Games/Sheetmusic/WorkingSheetmusic.cpp
+++ NeoKB_try/Games/Sheetmusic/WorkingSheetmusic.cpp
@@ -96,7 +96,7 @@ Sm<Event>* WorkingSm::createSm()
 string WorkingSm::getPathForFile(string fileName)
 {
 	string s = smInfo->fileInfo->Path + "/"s + fileName;
-	LOG(LogLevel::Finer) << "string WorkingSm::getPathForFile(string) : creating Sm from path [" << s << "] ...";
+	LOG(LogLevel::Fine) << "string WorkingSm::getPathForFile(string) : creating Sm from path [" << s << "] ...";
 	return s;
 }
 
