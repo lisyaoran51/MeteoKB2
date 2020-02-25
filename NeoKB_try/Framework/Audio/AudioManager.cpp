e8df901fc238fb89bb66d28534d02fefa243c58d
a0939946923 <a0939946923@gmail.com>
2020-02-25 15:50:01 +0800

    no message

--- NeoKB_try/Framework/Audio/AudioManager.cpp
+++ NeoKB_try/Framework/Audio/AudioManager.cpp
@@ -52,7 +52,7 @@ TrackManager * AudioManager::GetTrackManager(CompositeResourceStore<char*>* trac
 	AddItem(tManager);
 	tManager->AddAdjustment(AdjustableProperty::Volume, trackVolume);
 	
-	trackVolume->SetValue(1.0);
+	trackVolume->SetValue(0.0);
 	// TODO: ¥[¤Wadjistment
 
 	return tManager;
