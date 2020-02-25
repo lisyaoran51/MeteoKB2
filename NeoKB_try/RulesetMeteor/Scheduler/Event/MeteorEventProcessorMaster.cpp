d3649af7819c6b47b683997fc99b63f82c533761
a0939946923 <a0939946923@gmail.com>
2020-02-26 06:45:46 +0800

    no message

--- NeoKB_try/RulesetMeteor/Scheduler/Event/MeteorEventProcessorMaster.cpp
+++ NeoKB_try/RulesetMeteor/Scheduler/Event/MeteorEventProcessorMaster.cpp
@@ -36,7 +36,7 @@ int MeteorEventProcessorMaster::OnKeyDown(pair<MeteorAction, int> action)
 		if (noteControlPointHitObject == nullptr)
 			continue;
 
-		LOG(LogLevel::Debug) << "MeteorEventProcessorMaster::OnKeyDown() : checking hit object [" << (int)pitchBindings[noteControlPointHitObject->GetPitch()] << "] on [" << noteControlPointHitObject->GetStartTime() << "] matching [" << int(action.first) << "] on [" << currentTime << "].";
+		LOG(LogLevel::Debug) << "MeteorEventProcessorMaster::OnKeyDown() : checking hit object [" << (int)noteControlPointHitObject->GetPitch() << "] on [" << noteControlPointHitObject->GetStartTime() << "] matching [" << int(action.first) << "] on [" << currentTime << "].";
 
 		if (!matchPitch(noteControlPointHitObject, action.first))
 			continue;
