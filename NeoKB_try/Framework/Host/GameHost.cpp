c0b4935e53ba8ac91ba3a726b908f1d29ea50813
a0939946923 <a0939946923@gmail.com>
2020-03-04 02:35:22 +0800

    no message

--- NeoKB_try/Framework/Host/GameHost.cpp
+++ NeoKB_try/Framework/Host/GameHost.cpp
@@ -276,7 +276,7 @@ int GameHost::setupConfig()
 	frameworkConfigManager->Set(FrameworkSetting::BlackKeyTargetHeight, 10);	// 14
 	frameworkConfigManager->Set(FrameworkSetting::StartPitch, 24);
 	frameworkConfigManager->Set(FrameworkSetting::FrameRate, 30);
-	frameworkConfigManager->Set(FrameworkSetting::SongTitle, string("OldClock"));
+	frameworkConfigManager->Set(FrameworkSetting::SongTitle, string("OldClock_Left"));
 
 	dependencies->Cache<FrameworkConfigManager>(frameworkConfigManager);
 	
