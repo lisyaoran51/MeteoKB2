388c81c2c5303cc23d092c926aeedd67e67c6ba1
a0939946923 <a0939946923@gmail.com>
2020-05-12 02:54:36 +0800

    no message

--- NeoKB_try/Desktop/Devices/MeteoPanelDevice.cpp
+++ NeoKB_try/Desktop/Devices/MeteoPanelDevice.cpp
@@ -68,7 +68,7 @@ int MeteoPanelDevice::readFromDevice()
 
 	}
 
-	if (_debugCount == 1000 || _debugCount == 1001 || _debugCount == 1002 || _debugCount == 1003 || _debugCount == 1004) {
+	if (_debugCount == 1000 || _debugCount == 1100 || _debugCount == 1200 || _debugCount == 1300 || _debugCount == 1400) {
 		LOG(LogLevel::Debug) << "MeteoPanelDevice::readFromDevice() : Create fake pause input [SpeedKnob -].";
 		InputState* newState = new InputState();
 		newState->SetPanelState(new PanelState());
