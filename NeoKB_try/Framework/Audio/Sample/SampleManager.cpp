ba7e40efbd460f09dd3b25759cd1bcb95e0f5794
a0939946923 <a0939946923@gmail.com>
2020-07-17 18:36:11 +0800

    no message

--- NeoKB_try/Framework/Audio/Sample/SampleManager.cpp
+++ NeoKB_try/Framework/Audio/Sample/SampleManager.cpp
@@ -61,7 +61,8 @@ SampleChannel * SampleManager::GetMultiPlaybackSampleChannel(string name)
 		string path = resourceStore->GetFilePath(name);
 		if (path != "") {
 			sample = sampleCache[name] = new BassSample((char*)path.c_str());
-			sampleChannel = sampleChannelCache[name] = new MultiPlaybackBassSampleChannel(sample, playbackAmount, 2, OverrideType::MinimunVolume);
+			//sampleChannel = sampleChannelCache[name] = new MultiPlaybackBassSampleChannel(sample, playbackAmount, 2, OverrideType::MinimunVolume);
+			sampleChannel = sampleChannelCache[name] = new MultiPlaybackBassSampleChannel(sample, 20, 2, OverrideType::MinimunVolume);
 			AddItem(sampleChannel);
 		}
 		else {
