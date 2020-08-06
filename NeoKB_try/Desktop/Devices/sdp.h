6c4c72074aa1ffbd7ff52f28e3c35607337ded22
a0939946923 <a0939946923@gmail.com>
2020-08-07 00:09:39 +0800

    no message

--- NeoKB_try/Desktop/Devices/sdp.h
+++ NeoKB_try/Desktop/Devices/sdp.h
@@ -1,15 +1,15 @@
 #ifndef SDP_H
 #define SDP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/sdp.h>
 #include <bluetooth/sdp_lib.h>
 #include <bluetooth/rfcomm.h>
 #include <sys/socket.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 	extern sdp_session_t * sdp_register_service(int port);
 	extern int sdp_init_server(int port);
