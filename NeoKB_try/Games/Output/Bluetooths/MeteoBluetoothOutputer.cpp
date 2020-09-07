5d1dd641f41f4cf51ab08eaf539e84151678fb4c
a0939946923 <a0939946923@gmail.com>
2020-09-08 03:56:42 +0800

    no message

--- NeoKB_try/Games/Output/Bluetooths/MeteoBluetoothOutputer.cpp
+++ NeoKB_try/Games/Output/Bluetooths/MeteoBluetoothOutputer.cpp
@@ -1,7 +1,10 @@
 #include "MeteoBluetoothOutputer.h"
 
+#include "../../../Framework/IO/BluetoothPhone.h"
+
 
 using namespace Games::Output::Bluetooths;
+using namespace Framework::IO;
 
 
 int MeteoBluetoothOutputer::pushMessage(MeteoBluetoothMessage * outputMessage)
