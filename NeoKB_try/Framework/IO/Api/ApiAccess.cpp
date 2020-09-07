ddee3b7434be460e490c97cd67bdd328391b6e8d
a0939946923 <a0939946923@gmail.com>
2020-09-08 05:42:17 +0800

    no message

--- NeoKB_try/Framework/IO/Api/ApiAccess.cpp
+++ NeoKB_try/Framework/IO/Api/ApiAccess.cpp
@@ -18,12 +18,14 @@ int ApiAccess::AddOnStartWritingSmFile(_Type * callableObject, function<int(stri
 {
 	return gameHost->GetMainInterface()->GetBluetoothPhone()->AddOnStartWritingSmFile(callableObject, callback, name);
 }
+template int ApiAccess::AddOnStartWritingSmFile<MtoObject>(MtoObject * callableObject, function<int(string)> callback, string name);
 
 template<class _Type>
 int ApiAccess::AddOnWriteSmFileSuccess(_Type * callableObject, function<int(string)> callback, string name)
 {
 	return gameHost->GetMainInterface()->GetBluetoothPhone()->AddOnWriteSmFileSuccess(callableObject, callback, name);
 }
+template int ApiAccess::AddOnWriteSmFileSuccess<MtoObject>(MtoObject * callableObject, function<int(string)> callback, string name);
 
 template<class _Type>
 int ApiAccess::AddOnStartWritingSoundFilePackage(_Type * callableObject, function<int(string)> callback, string name)
