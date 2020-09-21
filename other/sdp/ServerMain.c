840a926738cd9af2efc63d1420b8525f630d2534
a0939946923 <a0939946923@gmail.com>
2020-09-22 07:19:53 +0800

    no message

--- other/sdp/ServerMain.c
+++ other/sdp/ServerMain.c
@@ -146,8 +146,8 @@ int main()
 				char msg[6] = "hello";
 				msg[5] = 10;
 				
-				//write(client, msg, 6);
-				//printf("s");printf("e");printf("n");printf("d");printf(".");printf("\n");
+				write(client, msg, 6);
+				printf("s");printf("e");printf("n");printf("d");printf(".");printf("\n");
                 bytes_read = read(client, buf, sizeof(buf));
                 if( bytes_read > 0 ) {
 					
