e71d4e4b3345c04c784b2d09b9179e7fe0e6ff2b
a0939946923 <a0939946923@gmail.com>
2021-01-22 01:29:13 +0800

    no message

--- NeoKB_try/Framework/Input/Handler/InputHandler.cpp
+++ NeoKB_try/Framework/Input/Handler/InputHandler.cpp
@@ -7,7 +7,7 @@ using namespace Framework::Input::Handler;
 
 vector<InputState*>* InputHandler::GetPendingStates()
 {
-	if (pendingStates.size() > 0)
+	//if (pendingStates.size() > 0)
 	//if (pendingStates[0]->GetBluetoothState() != nullptr) {
 	//	LOG(LogLevel::Depricated) << "InputHandler::GetPendingStates() : has pending bt input.";
 	//	LOG(LogLevel::Depricated) << "InputHandler::GetPendingStates() : has pending bt input [" << pendingStates[0]->GetBluetoothState()->GetCommands()->at(0)->GetContext().dump() << "].";
