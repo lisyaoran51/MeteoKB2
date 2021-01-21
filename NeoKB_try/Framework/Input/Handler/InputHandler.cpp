cc7ac6f8a6ebaf1b6723da6aac3f8b615834075a
a0939946923 <a0939946923@gmail.com>
2021-01-22 01:26:53 +0800

    no message

--- NeoKB_try/Framework/Input/Handler/InputHandler.cpp
+++ NeoKB_try/Framework/Input/Handler/InputHandler.cpp
@@ -1,19 +1,17 @@
 #include "InputHandler.h"
 
-#include "../Commands/BluetoothCommand.h"
 
 
 using namespace Framework::Input::Handler;
-using namespace Framework::Input::Commands;
 
 
 vector<InputState*>* InputHandler::GetPendingStates()
 {
 	if (pendingStates.size() > 0)
-	if (pendingStates[0]->GetBluetoothState() != nullptr) {
-		LOG(LogLevel::Depricated) << "InputHandler::GetPendingStates() : has pending bt input.";
-		LOG(LogLevel::Depricated) << "InputHandler::GetPendingStates() : has pending bt input [" << pendingStates[0]->GetBluetoothState()->GetCommands()->at(0)->GetContext().dump() << "].";
-	}
+	//if (pendingStates[0]->GetBluetoothState() != nullptr) {
+	//	LOG(LogLevel::Depricated) << "InputHandler::GetPendingStates() : has pending bt input.";
+	//	LOG(LogLevel::Depricated) << "InputHandler::GetPendingStates() : has pending bt input [" << pendingStates[0]->GetBluetoothState()->GetCommands()->at(0)->GetContext().dump() << "].";
+	//}
 
 	vector<InputState*>* states = new vector<InputState*>();
 	states->assign(pendingStates.begin(), pendingStates.end());
