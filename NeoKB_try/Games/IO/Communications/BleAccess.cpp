57927b78610375994cc1b2a17b546f8af002d766
a0939946923 <a0939946923@gmail.com>
2021-01-22 14:59:35 +0800

    no message

--- NeoKB_try/Games/IO/Communications/BleAccess.cpp
+++ NeoKB_try/Games/IO/Communications/BleAccess.cpp
@@ -115,12 +115,12 @@ int BleAccess::run()
 		request = communicationRequests.back();
 	
 		// request處理成功
-		if (handleRequest(request) >= 0) {
-			communicationRequests.pop_back();
-		}
-		else {
-			return -1;
-		}
+		//if (handleRequest(request) >= 0) {
+		//	communicationRequests.pop_back();
+		//}
+		//else 
+		//	return -1;
+		
 	}
 
 	return 0;
