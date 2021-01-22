14b3a5ebdb3773bfae5a8011730a6125ecd7ac71
a0939946923 <a0939946923@gmail.com>
2021-01-22 12:56:06 +0800

    no message

--- NeoKB_try/Framework/IO/Communications/CommunicationCollectionManager.h
+++ NeoKB_try/Framework/IO/Communications/CommunicationCollectionManager.h
@@ -77,15 +77,6 @@ namespace Communications{
 			return 0;
 		}
 
-
-		virtual int OnStateChange() {
-			AdjustableAudioComponent::OnStateChange();
-			unique_lock<mutex> uLock(itemMutex);
-			for (int i = 0; i < items.size(); i++) {
-				items[i]->OnStateChange();
-			}
-		}
-
 		int SetIsActive(bool iActive) {
 			isActive = iActive;
 		}
