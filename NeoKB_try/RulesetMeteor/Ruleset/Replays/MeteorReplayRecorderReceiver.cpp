48453a44302b907ccfa5abb2f2c536dc7beccc04
a0939946923 <a0939946923@gmail.com>
2021-03-02 06:44:47 +0800

    no message

--- NeoKB_try/RulesetMeteor/Ruleset/Replays/MeteorReplayRecorderReceiver.cpp
+++ NeoKB_try/RulesetMeteor/Ruleset/Replays/MeteorReplayRecorderReceiver.cpp
@@ -6,4 +6,5 @@ using namespace Meteor::Rulesets::Replays;
 
 MeteorReplayRecorderReceiver::MeteorReplayRecorderReceiver() : RegisterType("MeteorReplayRecorderReceiver")
 {
+	LOG(LogLevel::Debug) << "MeteorReplayRecorderReceiver::MeteorReplayRecorderReceiver() : constructing.";
 }
