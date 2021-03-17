b32f2d02a4ca75fc190bd4c5058426767dbd64c0
a0939946923 <a0939946923@gmail.com>
2021-03-18 04:41:53 +0800

    no message

--- NeoKB_try/Framework/IO/Communications/CommunicationRequest.h
+++ NeoKB_try/Framework/IO/Communications/CommunicationRequest.h
@@ -28,6 +28,44 @@ namespace Communications{
 
 	class CommunicationRequestException : public exception {
 
+		// °Ñ¦Ò¦Û https://stackoverflow.com/questions/8152720/correct-way-to-inherit-from-stdexception
+
+		CommunicationRequestException(){}
+
+		/** Constructor (C strings).
+		*  @param message C-style string error message.
+		*                 The string contents are copied upon construction.
+		*                 Hence, responsibility for deleting the char* lies
+		*                 with the caller.
+		*/
+		explicit CommunicationRequestException(const char* message)
+			: msg_(message) {}
+
+		/** Constructor (C++ STL strings).
+		 *  @param message The error message.
+		 */
+		explicit CommunicationRequestException(const std::string& message)
+			: msg_(message) {}
+
+		/** Destructor.
+		 * Virtual to allow for subclassing.
+		 */
+		virtual ~CommunicationRequestException() noexcept {}
+
+		/** Returns a pointer to the (constant) error description.
+		 *  @return A pointer to a const char*. The underlying memory
+		 *          is in posession of the Exception object. Callers must
+		 *          not attempt to free the memory.
+		 */
+		virtual const char* what() const noexcept {
+			return msg_.c_str();
+		}
+
+	protected:
+		/** Error message.
+		 */
+		std::string msg_;
+
 	};
 
 	class CommunicationRequest : public MtoObject {
