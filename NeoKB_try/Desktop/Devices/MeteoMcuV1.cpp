0d1187f3156592a9545c4c641cd4f7b9815b098f
a0939946923 <a0939946923@gmail.com>
2021-04-07 09:34:19 +0800

    no message

--- NeoKB_try/Desktop/Devices/MeteoMcuV1.cpp
+++ NeoKB_try/Desktop/Devices/MeteoMcuV1.cpp
@@ -244,7 +244,9 @@ int MeteoMcuV1::writePanel()
 		strcpy(cstr, i2cMessages[i].c_str());
 		cstr[i2cMessages[i].length()] = '\0';
 
-		LOG(LogLevel::Debug) << "MeteoPanelBoardV1::writePanel() : write [" << cstr << "](" << i2cMessages[i].c_str() << ") to i2c. last char is [" << cstr[i2cMessages[i].length()] << "] at " << i2cMessages[i].length();
+		LOG(LogLevel::Debug) << "MeteoMcuV1::writePanel() : write [" << cstr << "] to i2c." << i2cMessages.size();
+
+		LOG(LogLevel::Debug) << "MeteoMcuV1::writePanel() : write [" << cstr << "](" << i2cMessages[i].c_str() << ") to i2c. last char is [" << cstr[i2cMessages[i].length()] << "] at " << i2cMessages[i].length();
 
 		i2cInterface->i2cWrite(cstr, i2cMessages[i].length() + 1);
 
