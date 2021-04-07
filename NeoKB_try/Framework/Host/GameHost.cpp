fcca2a74c78c37273fec9e6b1f6b99eb2520da63
a0939946923 <a0939946923@gmail.com>
2021-04-08 01:54:33 +0800

    no message

--- NeoKB_try/Framework/Host/GameHost.cpp
+++ NeoKB_try/Framework/Host/GameHost.cpp
@@ -210,7 +210,7 @@ int GameHost::updateInitialize()
 {
 	LOG(LogLevel::Info) << "GameHost::updateInitialize() : Setting update thread.";
 	// ¹w³]¬O1000
-	updateThread->SetMaxUpdateHz(10);
+	updateThread->SetMaxUpdateHz(60);
 	return 0;
 }
 
