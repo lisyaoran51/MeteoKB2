1acde7457e3997e1fa18e89ee4eb447ca687b507
a0939946923 <a0939946923@gmail.com>
2021-04-08 02:36:23 +0800

    no message

--- NeoKB_try/Desktop/Devices/MeteoPanelDevice.cpp
+++ NeoKB_try/Desktop/Devices/MeteoPanelDevice.cpp
@@ -15,14 +15,14 @@ int MeteoPanelDevice::readFromDevice()
 	if (newState != nullptr)
 		inputStates.push_back(newState);
 #else
-	if (_debugCount % 2000 == 0 || _debugCount % 2000 == 501) {
+	if (_debugCount % 1000 == 0 || _debugCount % 1000 == 250) {
 		InputState* newState = new InputState();
 		newState->SetPanelState(new PanelState());
 
 		newState->GetPanelState()->AddKnob(pair<InputKey, int>(InputKey::SpeedKnob, 1));
 		inputStates.push_back(newState);
 	}
-	else if (_debugCount % 2000 == 1000 || _debugCount % 2000 == 1500) {
+	else if (_debugCount % 1000 == 500 || _debugCount % 1000 == 750) {
 		InputState* newState = new InputState();
 		newState->SetPanelState(new PanelState());
 
