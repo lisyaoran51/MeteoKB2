952cb8345f8847c36a51a41206034a7326dd181e
a0939946923 <a0939946923@gmail.com>
2021-04-27 23:44:25 +0800

    no message

--- NeoKB_try/RulesetMeteor/Scheduler/Event/TimeEvents/TimeControllerControllers/RepeatPracticeController.cpp
+++ NeoKB_try/RulesetMeteor/Scheduler/Event/TimeEvents/TimeControllerControllers/RepeatPracticeController.cpp
@@ -36,6 +36,7 @@ RepeatPracticeController::RepeatPracticeController(): RegisterType("RepeatPracti
 
 int RepeatPracticeController::implementControlTimeController(EventProcessor<Event>* e)
 {
+	LOG(LogLevel::Debug) << "RepeatPracticeController::implementControlTimeController() : repeat practice event [" << e << "].";
 	meteorTimeController->RepeatSection(dynamic_cast<RepeatPracticeEventProcessor*>(e)->GetRepeatSection());
 
 	return 0;
