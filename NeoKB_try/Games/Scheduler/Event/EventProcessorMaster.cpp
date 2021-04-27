f2658c9305477069138e95a8c4acc39eeb5aaa09
a0939946923 <a0939946923@gmail.com>
2021-04-27 22:16:31 +0800

    no message

--- NeoKB_try/Games/Scheduler/Event/EventProcessorMaster.cpp
+++ NeoKB_try/Games/Scheduler/Event/EventProcessorMaster.cpp
@@ -232,7 +232,7 @@ int EventProcessorMaster::processEvent(MTO_FLOAT elapsedTime)
 					InstrumentEventProcessorInterface* instrumentEventProcessor = dynamic_cast<InstrumentEventProcessorInterface*>(eventProcessors[i]);
 					if (instrumentEventProcessor) {
 						if (instrumentEventProcessor->GetStartTime() < currentTime && instrumentEventProcessor->GetIsTransferable()) {
-							LOG(LogLevel::Depricated) << "EventProcessorMaster::processEvent : found instrument event processor [" << instrumentEventProcessor->GetStartTime() << "].";
+							LOG(LogLevel::Debug) << "EventProcessorMaster::processEvent : found instrument event processor [" << instrumentEventProcessor->GetStartTime() << "].";
 							instrumentEventProcessor->ControlInstrument();
 						}
 					}
