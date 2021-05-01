57048c35f6152dd12896f26e676bfb8e3509ab6a
a0939946923 <a0939946923@gmail.com>
2021-05-01 23:09:25 +0800

    no message

--- NeoKB_try/RulesetMeteor/Timing/MeteorTimeController.cpp
+++ NeoKB_try/RulesetMeteor/Timing/MeteorTimeController.cpp
@@ -344,7 +344,7 @@ int MeteorTimeController::RepeatSection(int section)
 	if (tempRepeatCounts < maxRepeatCounts - 1) {
 
 		// 這個段落已經練完，開始練下一個段落
-		if (section - tempRepeatStartSection == maxSectionAmountForOneRepeat) {
+		if (section + 1 - tempRepeatStartSection == maxSectionAmountForOneRepeat) {
 			
 
 			if (tempRepeatCounts % 2 == 1) {
