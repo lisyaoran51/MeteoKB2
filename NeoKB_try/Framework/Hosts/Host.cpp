bbdecdcf24d6c86b9ac350b8ebaf645806f44bab
a0939946923 <a0939946923@gmail.com>
2021-05-24 00:24:41 +0800

    no message

--- NeoKB_try/Framework/Hosts/Host.cpp
+++ NeoKB_try/Framework/Hosts/Host.cpp
@@ -290,7 +290,7 @@ int Host::setupConfig()
 	frameworkConfigManager->Set(FrameworkSetting::UpdateRate, 100);
 	frameworkConfigManager->Set(FrameworkSetting::AudioUpdateRate, 1000);
 	frameworkConfigManager->Set(FrameworkSetting::InputRate, 1000);
-	frameworkConfigManager->Set(FrameworkSetting::SongTitle, string("SongForBrother"));
+	frameworkConfigManager->Set(FrameworkSetting::SongTitle, string("SmallStepDanceSong"));
 
 	dependencies->Cache<FrameworkConfigManager>(frameworkConfigManager);
 	
