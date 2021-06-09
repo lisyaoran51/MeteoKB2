0feaf9aac3396d0a36cf053bee4836d963de210d
a0939946923 <a0939946923@gmail.com>
2021-06-09 10:00:14 +0800

    no message

--- NeoKB_try/Framework/IO/Communications/CommunicationAccess.cpp
+++ NeoKB_try/Framework/IO/Communications/CommunicationAccess.cpp
@@ -9,13 +9,13 @@ CommunicationAccess::CommunicationAccess(Host * gHost) : RegisterType("Communica
 {
 	// 這個本來應該要從game thread拿，但是因為這次我們不用game thread，所以就自己生一個
 	sourceClock = new StopwatchClock();
-	sourceClock->Start();
+	//sourceClock->Start();
 	setHost(gHost);
 }
 
 int CommunicationAccess::Update()
 {
-	framedClock->ProcessFrame();
+	//framedClock->ProcessFrame();
 	return CommunicationCollectionManager<CommunicationComponent>::Update();
 }
 
