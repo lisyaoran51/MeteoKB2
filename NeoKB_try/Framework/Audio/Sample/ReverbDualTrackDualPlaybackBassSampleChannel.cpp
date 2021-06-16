32d1751b26bb12fa81c40aa461a29467e6c62e1e
a0939946923 <a0939946923@gmail.com>
2021-06-16 12:40:56 +0800

    no message

--- NeoKB_try/Framework/Audio/Sample/ReverbDualTrackDualPlaybackBassSampleChannel.cpp
+++ NeoKB_try/Framework/Audio/Sample/ReverbDualTrackDualPlaybackBassSampleChannel.cpp
@@ -77,11 +77,11 @@ int ReverbDualTrackDualPlaybackBassSampleChannel::Play()
 
 		// reverb sample
 		BASS_ChannelPause(reverbChannelID[newPlayback]);
-		BASS_ChannelSetAttribute(reverbChannelID[newPlayback], BASS_ATTRIB_VOL, volumeCalculated->GetValue() * initialVolume);// / 4.f);
+		BASS_ChannelSetAttribute(reverbChannelID[newPlayback], BASS_ATTRIB_VOL, volumeCalculated->GetValue() * initialVolume / 3);// / 4.f);
 		BASS_ChannelSetPosition(reverbChannelID[newPlayback], 0, BASS_POS_BYTE);
 		/* 檢查是否在fadeout，是的話把fadeout停掉 */
 		if (BASS_ChannelIsSliding(reverbChannelID[newPlayback], BASS_ATTRIB_VOL) == TRUE)
-			BASS_ChannelSlideAttribute(reverbChannelID[newPlayback], BASS_ATTRIB_VOL, volumeCalculated->GetValue() * initialVolume / 3, (DWORD)(0));// / 4.f, (DWORD)(0));
+			BASS_ChannelSlideAttribute(reverbChannelID[newPlayback], BASS_ATTRIB_VOL, volumeCalculated->GetValue() * initialVolume, (DWORD)(0));// / 4.f, (DWORD)(0));
 
 
 		BASS_ChannelPlay(channelID[newPlayback], false);
