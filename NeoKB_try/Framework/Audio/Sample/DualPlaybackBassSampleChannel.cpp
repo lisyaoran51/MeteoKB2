38c4b85f81ba80afa55b237ab2210ae105ac923e
a0939946923 <a0939946923@gmail.com>
2021-06-24 08:03:38 +0800

    no message

--- NeoKB_try/Framework/Audio/Sample/DualPlaybackBassSampleChannel.cpp
+++ NeoKB_try/Framework/Audio/Sample/DualPlaybackBassSampleChannel.cpp
@@ -92,9 +92,13 @@ int DualPlaybackBassSampleChannel::Play()
 
 int DualPlaybackBassSampleChannel::Play(double v)
 {
-	float tempChannelVolume = 0;
-	BASS_ChannelGetAttribute(channelID[tempPlayingPlayback], BASS_ATTRIB_VOL, &tempChannelVolume);
-	lastVolume = tempChannelVolume;
+	if (BASS_ChannelIsActive(channelID[tempPlayingPlayback]) == BASS_ACTIVE_PLAYING) {
+		float tempChannelVolume = 0;
+		BASS_ChannelGetAttribute(channelID[tempPlayingPlayback], BASS_ATTRIB_VOL, &tempChannelVolume);
+		lastVolume = tempChannelVolume;
+	}
+	else
+		lastVolume = 0;
 	volume->SetValue(v);
 	InvalidateState();
 	return Play();
--- NeoKB_try/Games/UI/SoundSelectPanel.cpp
+++ NeoKB_try/Games/UI/SoundSelectPanel.cpp
@@ -67,7 +67,7 @@ int SoundSelectPanel::firstLoadSound()
 		return 0;
 	}
 
-	string firstLoadSoundBankName = "V2JapaneseStudio";
+	string firstLoadSoundBankName = "Bosendorfer";
 
 	instrumentConfigManager->Get(InstrumentSetting::InitialSoundBankName, &firstLoadSoundBankName);
 
