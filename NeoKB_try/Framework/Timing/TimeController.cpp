388f50426739551072dec4d3ea29590faa603344
a0939946923 <a0939946923@gmail.com>
2021-07-01 23:36:34 +0800

    no message

--- NeoKB_try/Framework/Timing/TimeController.cpp
+++ NeoKB_try/Framework/Timing/TimeController.cpp
@@ -141,6 +141,7 @@ TimeController::~TimeController()
 
 int TimeController::SetControllableClock(AdjustableClock * cClock)
 {
+	LOG(LogLevel::Debug) << "TimeController::SetControllableClock() : error here? " << cClock << " " << controllableClock;
 	controllableClock = cClock;
 	return 0;
 }
