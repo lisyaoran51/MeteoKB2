9a0feab9415d0fd51ec52f7e6ebb17e48df712eb
a0939946923 <a0939946923@gmail.com>
2021-07-16 16:47:09 +0800

    no message

--- NeoKB_try/RulesetInstant/Scheduler/Event/Effect/InstantSpotEffectMapper.cpp
+++ NeoKB_try/RulesetInstant/Scheduler/Event/Effect/InstantSpotEffectMapper.cpp
@@ -1,1 +1,9 @@
 #include "InstantSpotEffectMapper.h"
+
+#include "InstantSpotEffect.h"
+
+using namespace Instant::Schedulers::Events::Effects;
+
+InstantSpotEffectMapper::InstantSpotEffectMapper(int w, int h) : EffectMapper<InstantSpotEffect>(w, h)
+{
+}
